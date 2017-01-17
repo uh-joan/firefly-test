2a0d6f7f5eba092147012e605e868bd26511d881
Juan Saez <joan@retechnica.com>
2017-01-17 16:52:14 +0000

    deploy to heroku

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
+var gzippo = require('gzippo');
 var express = require('express');
+var morgan = require('morgan');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-    response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
